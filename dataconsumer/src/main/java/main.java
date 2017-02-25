f7500f71a68ca5defcc1970cd26b3e92440c6d46
miketwenty1 <mtidwell021@gmail.com>
2017-02-25 13:48:58 -0500

    adding java data producer

---
+++ dataconsumer/src/main/java/main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by tidwell on 2/25/17.
+ */
+class DataProducer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+}
