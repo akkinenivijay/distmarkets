c6a2707f73d00c061e818895078248f2768dfb5a
miketwenty1 <mtidwell021@gmail.com>
2017-02-25 15:49:51 -0500

    express app

--- dataconsumer/src/main/java/main.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by tidwell on 2/25/17.
- */
-class DataProducer {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
-    }
-}
