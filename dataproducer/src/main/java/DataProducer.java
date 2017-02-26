23b5f028801fa1a7afd183db55a889811eb2c978
miketwenty1 <mtidwell021@gmail.com>
2017-02-25 19:50:33 -0500

    producer update

--- dataproducer/src/main/java/DataProducer.java
+++ dataproducer/src/main/java/DataProducer.java
@@ -5,23 +5,37 @@ import org.json.simple.parser.JSONParser;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Random;
 
 public class DataProducer {
 
     @SuppressWarnings("unchecked")
     public static void main(String[] args) throws IOException {
 
+        String file = args[0];
+        //String file = "/Users/tidwell/Downloads/iotsmall.json";
+
         String requestUrl = "http://localhost:8080/distmarkets/data";
         JSONParser parser = new JSONParser();
         try {
-            JSONArray jsonArray = (JSONArray) parser.parse(new FileReader("/Users/tidwell/Downloads/iotsmall.json"));
-
-
+            JSONArray jsonArray = (JSONArray) parser.parse(new FileReader(file));
             for (int i = 0 ; i < jsonArray.size(); i++) {
+                String id = java.util.UUID.randomUUID().toString();
+                String timestamp = new Timestamp(System.currentTimeMillis()).toString();
                 JSONObject obj = (JSONObject) jsonArray.get(i);
+                obj.put("Timestamp", timestamp);
+                obj.put("INSPECTION_ID", obj.get("INSPECTION_ID").toString()+"-"+id);
                 String objstr = obj.toString();
                 sendPostRequest(requestUrl, objstr);
                 System.out.println(objstr);
+                Random r = new Random();
+                int Low = 1;
+                int High = 7500;
+                int Result = r.nextInt(High-Low) + Low;
+
+                Thread.sleep(Result);
                 //System.out.println(obj.toJSONString());
                 //String A = (String) obj.get("LOCATION_QUALITY");
                 //Long B = (Long) obj.get("INSPECTION_SCORE");
