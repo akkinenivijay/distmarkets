cd13cb3950f4ae5a04daa59b58c1c8a45c979286
Vijay Akkineni <vakkineni@liaison.com>
2017-02-25 23:54:54 -0500

    geo query

--- distmarkets-service/src/distmarkets_service/api_routes.clj
+++ distmarkets-service/src/distmarkets_service/api_routes.clj
@@ -4,12 +4,22 @@
             [yada.yada :as yada]
             [cheshire.core :as cheshire-core]
             [distmarkets-service.topics :as topics]
+            [distmarkets-service.redis :as redis]
             [yada.swagger :as swagger]))
 
 (defn capture-data
   [ctx] 
   (topics/put "data-topic" (cheshire-core/generate-string (:body ctx)))
   {:status :success})
+
+(defn geo-query
+  [ctx]
+  (redis/geo-radius-query))
+
+(defn id-data
+  [ctx]
+  (println ctx)
+  (redis/get-data-info (get-in ctx [:request :parameters :path :id])))
 
 (defn data-resource
   "yada resource for posting data"
@@ -21,11 +31,34 @@
     :produces    "application/json"
     :methods     {:post {:response capture-data}}}))
 
+(defn geo-resource
+  "yada resource for geo query"
+  []
+  (yada/resource
+   {:id          :distmarkets-service/geo
+    :description "Endpoint to geo query."
+    :consumes    "application/json"
+    :produces    "application/json"
+    :methods     {:get {:response geo-query}}}))
+
+(defn id-resource
+  "yada resource for posting data"
+  []
+  (yada/resource
+   {:id          :distmarkets-service/id
+    :description "Endpoint to get id."
+    :parameters  {:path {:id String}}
+    :consumes    "application/json"
+    :produces    "application/json"
+    :methods     {:get {:response id-data}}}))
+
 (defn api-routes []
   "Defines the distmarkets service rest API paths following the bidi syntax" 
   (let [routes
         ["/distmarkets"
-         [["/data" (data-resource)]]] 
+         [["/data" (data-resource)]
+          ["/geo" (geo-resource)]
+          ["/" :id (id-resource)]]] 
         swagger-setup
         ["/distmarkets/swagger.json"
          (bidi/tag
